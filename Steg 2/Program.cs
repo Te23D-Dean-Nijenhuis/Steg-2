4149f584e5d34e7316692b02f79e36db9958cda5
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-11-18 09:52:06 +0100

    asd

---
+++ Steg 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+string[] saker = ["leksak1","leksak2","leksak3","leksak4","leksak5"];
+
+string[] namn = ["Dean","Rasmus","Martin","Joakim","Anton"];
+
+int[] nummer = [1,3,5,7,9];
+
+for (int i = 0; i < 5; i++)
+{
+    int r = Random.Shared.Next(0,5);
+    int r2 = Random.Shared.Next(0,5);
+    int r3 = Random.Shared.Next(0,5);
+
+    Console.WriteLine($"{namn[r]} ger {saker[r2]} betyget {nummer[r3]}");
+}
+
+Console.ReadLine();
